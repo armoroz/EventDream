de2932683b6bb682628e4abaaec09907ee5481f2
Shalini <B00128436@mytudublin.ie>
2023-02-08 20:41:47 +0000

    roles and permissions step 6 - using roles to control what the user sees on login

---
+++ resources/views/layouts/adminmenu.blade.php
@@ -0,0 +1,15 @@
+<li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin Menu
+    <span class="caret"></span></a>
+    <ul class="dropdown-menu">
+        <li>
+             <a href="{!! route('users.index') !!}">Users</a>
+        </li>
+        <li>
+             <a href="{!! route('roles.index') !!}">Roles</a>
+        </li>
+        <li>
+             <a href="{!! route('permissions.index') !!}">Permissions</a>
+        </li>
+    </ul>
+</li>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <!-- Bootstrap 3.3.7 --> 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
         <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
 		<link rel="stylesheet" href="{{asset('css/app.css')}}">
   
@@ -21,7 +23,17 @@
 			<li><a href="{{route('display.index')}}" style="font-size: 12pt" >Calendar</a></li>
 			<li><a href="{{route('customers.index')}}" style="font-size: 12pt" >Customers</a></li>
 			<li><a href="{{route('bookings.index')}}" style="font-size: 12pt" >Bookings</a></li>
-			@include('layouts.navAuth')
+			<ul class="nav navbar-nav navbar-right" style="margin-right:10px">
+				@if(Auth::check())
+					@if(Auth::user()->hasRole('System Admin'))
+						 @include('layouts.adminmenu')
+					@endif 
+					<li><a href="{!! route('logout') !!}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+				@else
+					<li><a href="{!! route('login') !!}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					<li><a href="{!! route('register') !!}"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+				@endif
+			</ul>
         </nav> 
         <div id="page-content-wrapper"> 
             <div class="container-fluid"> 
