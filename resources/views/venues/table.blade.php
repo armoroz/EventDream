9e2153a66304051167d7bc5b896a249933fde0bd
Shalini <B00128436@mytudublin.ie>
2023-02-11 21:36:38 +0000

    adding star rating for venue - step 3 - select which venue to rate

--- resources/views/venues/table.blade.php
+++ resources/views/venues/table.blade.php
@@ -35,6 +35,10 @@
                            class='btn btn-default btn-xs'>
                             <i class="far fa-edit"></i>
                         </a>
+						<a href="{!! route('venueratings.ratevenue', [$venue->id]) !!}" 
+						   class='btn btn-default btn-xs'>
+						   <i class="glyphicon glyphicon-ok"></i>
+						</a>
                         {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
                     {!! Form::close() !!}
