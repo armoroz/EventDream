d2e310a9176fbe3cb14ea9c7f98238d60e508525
Shalini <B00128436@mytudublin.ie>
2023-02-11 22:33:51 +0000

    adding star rating for venue - step 4 - add star rating component

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,7 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+require('jquery/dist/jquery.js');
+require('bootstrap3/dist/js/bootstrap.js');
+require('bootstrap-star-rating/js/star-rating.js');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <title>EventDream</title> 
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'> 
         <!-- Bootstrap 3.3.7 --> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
-        <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+        <script src="{{asset('js/app.js')}}"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
