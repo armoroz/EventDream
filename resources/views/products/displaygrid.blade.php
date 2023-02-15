c2814639b7325b75063775fb3b7b06ea9e0e95cc
arinamaros <b00128390@mytudublin.ie>
2023-02-15 20:21:25 +0000

    added css in correct file

--- resources/views/products/displaygrid.blade.php
+++ resources/views/products/displaygrid.blade.php
@@ -32,7 +32,7 @@
     @if ($j==0) <div class='row'> @endif 
         <div class="col-sm-4">
             <div class="panel panel-primary"> 
-            <div class="panel-heading" style="background-color:#2d3033;">{{ $product->productname }} {{ $product->productdesc }} {{ $product->producttype }}</div> 
+            <div class="panel-heading">{{ $product->productname }} {{ $product->productdesc }} {{ $product->producttype }}</div> 
             <div class="panel-body"><img style="width:80%;height:200px;" class="img-responsive center-block" src="{{ asset('/img/' . $product->productimg)}}"/></div> 
             <div class="panel-footer"><button id="addItem" type="button" class="btn btn-default center-block addItem" value="{{$product->id}}">Add To Cart</button></div>
         </div>
