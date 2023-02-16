0111275126e598ddf5dce423415d53ae5b9d04cb
arinamaros <b00128390@mytudublin.ie>
2023-02-16 00:28:35 +0000

    fixed more css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,11 @@
    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	-->
-			
-		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>	
+	-->	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>	
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>	
 		<link rel="stylesheet" href="{{asset('css/app.css')}}"> 
 		<script src="{{asset('js/app.js')}}"></script>
     </head> 
--- routes/web.php
+++ routes/web.php
@@ -22,64 +22,50 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-//require __DIR__ ."/auth.php";
 
-Route::resource('customers', App\Http\Controllers\customerController::class);
-Route::resource('calendar/display', App\Http\Controllers\calendarController::class);
-
-//Route::resource('products', App\Http\Controllers\productController::class);
+Route::get('/logout','\App\Http\Controllers\Auth\AuthenticatedSessionController@destroy');
+Route::post('login','App\Http\Controllers\Auth\AuthenticatedSessionController@store');
+Route::get('/loggedInCustomer','App\Http\Controllers\customerController@getLoggedInCustomerDetails');
 
 Route::get('products/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
 Route::get('products', 'App\Http\Controllers\productController@index')->name('products.index');
-/*Route::resource('employees', App\Http\Controllers\employeeController::class);*/
-
 Route::get('products/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
-
 Route::get('products/emptycart', 'App\Http\Controllers\productController@emptycart')->name('products.emptycart');
 
 Route::get('events/checkout', 'App\Http\Controllers\eventController@checkout')->name('events.checkout');
-
 Route::resource('events', App\Http\Controllers\eventController::class);
-
 Route::resource('eventproductlogs', App\Http\Controllers\eventproductlogController::class);
-
 Route::post('events/placeorder', 'App\Http\Controllers\eventController@placeorder')->name('events.placeorder');
 
+Route::resource('customers', App\Http\Controllers\customerController::class);
 Route::resource('bookings', App\Http\Controllers\bookingController::class);
-
-//Route::get('/home','App\Http\Controllers\customerController@index')->name('home');
-//Route::get('logout','App\Http\Controllers\Auth\LoginController@logout')->name('logout');
-//Auth::routes();
-Route::get('/logout','\App\Http\Controllers\Auth\AuthenticatedSessionController@destroy');
-
-Route::post('login','App\Http\Controllers\Auth\AuthenticatedSessionController@store');
-
-Route::get('/loggedInCustomer','App\Http\Controllers\customerController@getLoggedInCustomerDetails');
-
+Route::resource('calendar/display', App\Http\Controllers\calendarController::class);
+Route::resource('venues', App\Http\Controllers\venueController::class);
+Route::resource('venueratings', App\Http\Controllers\venueratingController::class);
+Route::get('/venueratings/ratevenue/{venue}','App\Http\Controllers\venueratingController@ratevenue')->name('venueratings.ratevenue');
+Route::get('/venueratings/venue/{venue}','App\Http\Controllers\venueratingController@showvenueratings')->name('venueratings.showvenueratings');
 
 Route::resource('users', App\Http\Controllers\usersController::class);
-
-
-Route::resource('roles', App\Http\Controllers\rolesController::class);
-
-Route::get('/users/assignroles/{id}', 'App\Http\Controllers\UsersController@assignRoles')->name('users.assignroles');
-Route::patch('/users/updateroles/{id}', 'App\Http\Controllers\UsersController@updateRoles')->name("roles.rolesupdate");
-
 Route::resource('roles', App\Http\Controllers\rolesController::class);
 Route::resource('permissions', App\Http\Controllers\permissionsController::class);
-
+Route::get('/users/assignroles/{id}', 'App\Http\Controllers\UsersController@assignRoles')->name('users.assignroles');
+Route::patch('/users/updateroles/{id}', 'App\Http\Controllers\UsersController@updateRoles')->name("roles.rolesupdate");
 Route::get('/roles/assignpermissions/{id}', 'App\Http\Controllers\RolesController@assignPermissions')->name('roles.assignpermissions');
 Route::patch('/roles/updatepermissions/{id}', 'App\Http\Controllers\RolesController@updatePermissions')->name("roles.permissionsupdate");
 
-//Route::get('products', 'App\Http\Controllers\productController@index');
+/* System Admin Security */
 
-Route::group(['middleware' => ['role:System Admin']], function () {                           
+Route::group(['middleware' => ['role:System Admin']], function () {  
+//Route::resource('products', App\Http\Controllers\productController::class);
+//Route::get('products', 'App\Http\Controllers\productController@index');                     
 Route::get('/products/create', 'App\Http\Controllers\productController@create')->name('products.create')->middleware('permission:Create Product');
 Route::delete('/products/{product}', 'App\Http\Controllers\productController@destroy')->name('products.destroy')->middleware('permission:Delete Product');
 Route::get('products/{product}', 'App\Http\Controllers\productController@show')->name('products.show');
 Route::get('products/{product}/edit', 'App\Http\Controllers\productController@edit')->name('products.edit');
 Route::post('products','App\Http\Controllers\productController@store')->name('products.store');
 });
+
+/* Warehouse Worker Security */
 
 Route::group(['middleware' => ['role:Warehouse Worker']], function () { 
 Route::get('/products/create', 'App\Http\Controllers\productController@create')->name('products.create')->middleware('permission:Create Product');
@@ -89,13 +75,3 @@ Route::get('products/{product}/edit', 'App\Http\Controllers\productController@ed
 Route::post('products','App\Http\Controllers\productController@store')->name('products.store');
 Route::post('products/{product}/update','App\Http\Controllers\productController@update')->name('products.update');
 });
-
-Route::resource('venues', App\Http\Controllers\venueController::class);
-
-
-Route::resource('venueratings', App\Http\Controllers\venueratingController::class);
-
-Route::get('/venueratings/ratevenue/{venue}','App\Http\Controllers\venueratingController@ratevenue')->name('venueratings.ratevenue');
-
-Route::get('/venueratings/venue/{venue}','App\Http\Controllers\venueratingController@showvenueratings')->name('venueratings.showvenueratings');
-
