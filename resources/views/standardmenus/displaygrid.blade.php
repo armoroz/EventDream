bfde1590fbfa82dbfc956e32be5d061cd01678b5
Shalini <B00128436@mytudublin.ie>
2023-03-02 16:21:34 +0000

    added javascript for adding standard menu to cart and empty cart

--- resources/views/standardmenus/displaygrid.blade.php
+++ resources/views/standardmenus/displaygrid.blade.php
@@ -49,3 +49,35 @@
     @php $j++ @endphp 
     @if ($j==3) @php $j=0 @endphp </div> @endif 
 @endforeach
+
+<script>
+$(".bth,.addItem").click(function() {
+    var total = parseInt($('#shoppingcart').text());
+    var i=$(this).val();
+    $('#shoppingcart').text(total);    
+    $.ajax({
+      type: "get",
+      url: "{{url('standardmenus/additem/')}}" + "/" + i,
+      type: "GET",
+      success: function(response) {
+          total=total+1;
+          $('#shoppingcart').text(response.total);
+      },
+      error: function() {
+          alert("problem communicating with the server");
+      }
+    });
+});
+
+$("#emptycart").click(function() { $.ajax({ 
+    type: "get", url: "{{ url('standardmenus/emptycart')   }}",
+    success: function() { 
+        $('#shoppingcart').text(0); 
+    }, 
+    error: function() { 
+        alert("problem communicating with the server");
+    } 
+  }); 
+}); 
+</script>
+@endsection('content')
