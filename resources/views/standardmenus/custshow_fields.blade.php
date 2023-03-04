391f0223943cc78bd7a191474078128449415e2c
Shalini <B00128436@mytudublin.ie>
2023-03-04 13:28:45 +0000

    added custshow.blade for standardmenus

---
+++ resources/views/standardmenus/custshow_fields.blade.php
@@ -0,0 +1,34 @@
+<!-- Productname Field -->
+<div class="col-sm-12">
+    {!! Form::label('productname', 'Productname:') !!}
+    <p>{{ $product->productname }}</p>
+</div>
+
+<!-- Producttype Field -->
+<div class="col-sm-12">
+    {!! Form::label('producttype', 'Producttype:') !!}
+    <p>{{ $product->producttype }}</p>
+</div>
+
+<!-- Productdesc Field -->
+<div class="col-sm-12">
+    {!! Form::label('productdesc', 'Productdesc:') !!}
+    <p>{{ $product->productdesc }}</p>
+</div>
+
+<!-- Productcost Field -->
+<div class="col-sm-12">
+    {!! Form::label('productcost', 'Productcost:') !!}
+    <p>{{ $product->productcost }}</p>
+</div>
+
+<!-- Productquantity Field -->
+<div class="col-sm-12">
+    {!! Form::label('productquantity', 'Productquantity:') !!}
+    <p>{{ $product->productquantity }}</p>
+</div>
+
+<!-- Productimg Field -->
+<div><td><img class="img-responsive left-block" 
+	height="200px" width="200px" src="{{ $product->productimg }}">
+</td></div>
