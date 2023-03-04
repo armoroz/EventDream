4c707d4818fc3a13d23bbdffaebadd9aae5e0d3a
Shalini <B00128436@mytudublin.ie>
2023-03-04 13:37:21 +0000

    fixed custshow.blade error and added custshow_fields.blade for standardmenus

--- resources/views/standardmenus/custshow_fields.blade.php
+++ resources/views/standardmenus/custshow_fields.blade.php
@@ -1,34 +1,30 @@
-<!-- Productname Field -->
+<!-- Standardmenuname Field -->
 <div class="col-sm-12">
-    {!! Form::label('productname', 'Productname:') !!}
-    <p>{{ $product->productname }}</p>
+    {!! Form::label('standardmenuname', 'Standardmenuname:') !!}
+    <p>{{ $standardmenu->standardmenuname }}</p>
 </div>
 
-<!-- Producttype Field -->
+<!-- Style Field -->
 <div class="col-sm-12">
-    {!! Form::label('producttype', 'Producttype:') !!}
-    <p>{{ $product->producttype }}</p>
+    {!! Form::label('style', 'Style:') !!}
+    <p>{{ $standardmenu->style }}</p>
 </div>
 
-<!-- Productdesc Field -->
+<!-- Course Field -->
 <div class="col-sm-12">
-    {!! Form::label('productdesc', 'Productdesc:') !!}
-    <p>{{ $product->productdesc }}</p>
+    {!! Form::label('course', 'Course:') !!}
+    <p>{{ $standardmenu->course }}</p>
 </div>
 
-<!-- Productcost Field -->
+<!-- Description Field -->
 <div class="col-sm-12">
-    {!! Form::label('productcost', 'Productcost:') !!}
-    <p>{{ $product->productcost }}</p>
+    {!! Form::label('description', 'Description:') !!}
+    <p>{{ $standardmenu->description }}</p>
 </div>
 
-<!-- Productquantity Field -->
+<!-- Userid Field -->
 <div class="col-sm-12">
-    {!! Form::label('productquantity', 'Productquantity:') !!}
-    <p>{{ $product->productquantity }}</p>
+    {!! Form::label('userid', 'Userid:') !!}
+    <p>{{ $standardmenu->userid }}</p>
 </div>
 
-<!-- Productimg Field -->
-<div><td><img class="img-responsive left-block" 
-	height="200px" width="200px" src="{{ $product->productimg }}">
-</td></div>
