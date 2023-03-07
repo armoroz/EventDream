860b2dce312430460a83fbcab8bd1a8921f64647
Your Name <you@example.com>
2023-03-07 22:58:41 +0000

    added faq

--- resources/views/Aboutus/index.blade.php
+++ resources/views/Aboutus/index.blade.php
@@ -18,6 +18,18 @@
     <p>Our mission at the event management system is to create unforgettable experiences for our clients and their guests. We strive to deliver exceptional service, innovative solutions, and attention to detail in every aspect of event planning and execution. Our goal is to exceed our clients' expectations by providing personalized experiences that reflect their vision and objectives. We are committed to making each event unique, memorable, and successful, and we aim to achieve this through collaboration, creativity, and professionalism. Our mission is to be the go-to event management system for all types of events, from corporate meetings to social gatherings, and to make a positive impact on our clients' lives and communities..</p>
   </section>
 
+
+  <section>
+    <h2> FAQ</h2>
+    <p>    Can I visit the venue before booking?</p>
+    <p>  Yes, we encourage clients to visit the venue before booking to ensure it meets their needs. You can schedule a visit through our website.  </p>
+	<p>  What is your cancellation policy?</p>
+	<p>	Our cancellation policy varies based on the event and the venue. We will work with you to determine the best course of action in the event of a cancellation.</p>
+	<p> What kind of events do you manage </p>
+	<p>We manage a variety of events, from weddings and corporate events to concerts and festivals. Our team is experienced in managing events of all sizes and types.</p>
+  </section>
+
+
   <section>
     <h2>Team</h2>
     <ul>
@@ -32,10 +44,12 @@
 
 
 </p>
+
   </section>
 
   <footer>
     <p>Event Dream. All rights reserved.</p>
+	
   </footer>
 </body>
 </html>
