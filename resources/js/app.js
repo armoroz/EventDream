6969fbc666bfe03192950c315297bd1fc256262d
arinamaros <b00128390@mytudublin.ie>
2023-03-29 11:59:53 +0100

    fixed star rating css pulling twice

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ require('./bootstrap');
 //require('bootstrap3/dist/js/bootstrap.js');
 require('jquery/dist/jquery.js');
 require('bootstrap/dist/js/bootstrap.js');
-require('bootstrap-star-rating/js/star-rating.js');
 require('leaflet/dist/leaflet.js');
 global.$ = global.jQuery = require('jquery');
 
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -70,7 +70,7 @@ div.scrollmenu a:hover {
 				<div><a href="{{ route('venueratings.showvenueratings', [$venue->id] )}}">
 					<input id="fieldRating" name="rating" 
 					value="{!! round($venue->venueratings->avg('rating'),2); !!}" 
-					type="text" data-theme="krajee-fas" class="rating rating-loading" data-min=0 
+					type="text" data-theme="krajee-fas" class="rating" data-min=0 
 					data-max=5 data-step=1 data-size="sm" data-display-only="true">
 			     </a> </div>
 
