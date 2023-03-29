a966f44363872451aced12d4291527dfc46e90e8
Shalini <B00128436@mytudublin.ie>
2023-03-29 01:28:25 +0100

    created css cards for custom menus displaygrid

--- resources/views/standardmenus/displaygrid.blade.php
+++ resources/views/standardmenus/displaygrid.blade.php
@@ -23,34 +23,30 @@
 	<meta name= "viewport" content= "width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 </head>
-    <!--<div class='d-flex flex-wrap align-content-start bg-light'>-->
-    @foreach($standardmenus as $standardmenu) 
-        <!--<div class="p-2 border col-4 g-3"> 
-            <div class="card text-center">-->
-				<div class= "dpg-bodymenuoptions">
-				  <div class= "dpg-container">
-					<div class="dpg-menucard">
-						<div class="dpg-box-image">
-							@foreach($standardmenu->standardmenuimages->take(1) as $standardmenuimage)<img src="data:image/jpeg;base64,{{$standardmenuimage->imagefile}}">@endforeach
-						</div>
-						<div class="dpg-content">
-						<div class="card-header d-block"><h5 class="mx-auto d-block">{{ $standardmenu->standardmenuname }} {{ $standardmenu->standardmenudesc }} {{ $standardmenu->standardmenutype }}</h5></div>
-						<div class="card-footer" style="text-align: center">€{{$standardmenu->standardmenucost}}</div>
-						<div class="card-footer"><button id="addItem" type="button" class="btn btn-success mx-auto d-block addItem" value="{{$standardmenu->id}}">Add To Cart</button></div>
-						<div class="card-footer"><a  href="{{ route('standardmenus.custshow', [$standardmenu->id]) }}"><button id="custshow" type="button" class="btn btn-default center-block custshow">Details</button></a></div>
-						<div class="card-footer"><a href="{{ route('standardmenuratings.showstandardmenuratings', [$standardmenu->id] )}}">
-							<input id="fieldRating" name="rating" 
-							value="{!! round($standardmenu->standardmenuratings->avg('rating'),2); !!}" 
-							type="text" data-theme="krajee-fas" class="rating rating-loading" data-min=0 
-							data-max=5 data-step=1 data-size="sm" data-display-only="true"></a>
-						</div>		
-			        </div> 
-		        </div>
+
+	<div class= "dpg-bodyoptions">@foreach($standardmenus as $standardmenu) 
+	  <div class= "dpg-container">
+		<div class="dpg-card">
+			<div class="dpg-box-image">
+				@foreach($standardmenu->standardmenuimages->take(1) as $standardmenuimage)<img src="data:image/jpeg;base64,{{$standardmenuimage->imagefile}}">@endforeach
+			</div>
+			<div class="dpg-content">
+			<div class="card-header d-block"><h5 class="mx-auto d-block">{{ $standardmenu->standardmenuname }} {{ $standardmenu->standardmenudesc }} {{ $standardmenu->standardmenutype }}</h5></div>
+			<div class="card-footer" style="text-align: center">€{{$standardmenu->standardmenucost}}</div>
+			<div class="card-footer"><button id="addItem" type="button" class="btn btn-success mx-auto d-block addItem" value="{{$standardmenu->id}}">Add To Cart</button></div>
+			<div class="card-footer"><a  href="{{ route('standardmenus.custshow', [$standardmenu->id]) }}"><button id="custshow" type="button" class="btn btn-default center-block custshow">Details</button></a></div>
+			<div class="card-footer"><a href="{{ route('standardmenuratings.showstandardmenuratings', [$standardmenu->id] )}}">
+				<input id="fieldRating" name="rating" 
+				value="{!! round($standardmenu->standardmenuratings->avg('rating'),2); !!}" 
+				type="text" data-theme="krajee-fas" class="rating rating-loading" data-min=0 
+				data-max=5 data-step=1 data-size="sm" data-display-only="true"></a>
+			</div>		
 			</div> 
-		</div> 
-</html>
+		</div>
+	  </div> 
     @endforeach
-    
+	</div> 
+</html>
 
 
 
