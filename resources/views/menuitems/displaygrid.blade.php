5b99fef99c061b78d914db8a6e41cd0997f1945b
Shalini <B00128436@mytudublin.ie>
2023-03-30 14:08:32 +0100

    changed css of menu items displaygrid

--- resources/views/menuitems/displaygrid.blade.php
+++ resources/views/menuitems/displaygrid.blade.php
@@ -3,6 +3,19 @@
 @include('flash::message') 
 @php $j=0 @endphp 
  
+<section class="content-header">
+	<div class="container-fluid">
+		<div class="row mb-2">
+			<div class="col-sm-6" style="padding-bottom: 60px;">
+				<h1>Menu Items</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+
 <style>
     .fixed-button {
         position: fixed;
@@ -22,31 +35,38 @@
     }
 </style>
 
-<!--<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>-->
 <form action="{{route('menuitems.newstandardmenu')}}" id="newmenuform" method="post">
 @csrf
-<div class='d-flex flex-wrap align-content-start bg-light'> 
+<div class='d-flex flex-wrap align-content-start' style="margin:-100px"> 
 @foreach($menuitems as $menuitem) 
-        <div class="p-2 border col-4 g-3">
-            <div class="card text-center"> 
-            <div class="card-header d-block">{{ $menuitem->menuitemname }} {{ $menuitem->course }}</div> 
-			<div class="card-body"><img class="img-responsive center-block" height="100" width="100%" src="{{ $menuitem->menuitemimglink }}"></div>
+  <div class="p-0 col-4 g-4">
+	<div class= "bodyoptions-meni">
+	  <div class= "container-meni">
+		<div class="card-meni" style="height: 100%;">
+			<div class="box-image-meni">
+				<img src="{{ $menuitem->menuitemimglink }}">
+			</div>
+			<div class="content-meni">
+			<div class="card-header d-block"><h5 class="mx-auto d-block">{{ $menuitem->menuitemname }}</h5></div>
             <div class="card-footer">
                 <div class="btn-checkbox">
                     <div class="text-center">
-                        <a href="{{ route('menuitems.custshow', [$menuitem->id]) }}"><button id="custshow" type="button" class="btn btn-default center-block custshow">Details</button></a>
+						<a  href="{{ route('menuitems.custshow', [$menuitem->id]) }}"><button id="custshow" type="button" class="btn btn-default center-block custshow">More info <i class="fas fa-info-circle"></i></button></a>
                     </div>
                     <div class="checkbox">
                         <input type="checkbox" name="menuitems[]" value="{{ $menuitem->id }}">
                     </div>
                 </div>
-            </div>	
-        </div>
+            </div>
+			</div> 
+		</div>
+	  </div> 
 	</div>
-@endforeach
+ </div> 
+ @endforeach
 </div>
 
-    <button type="button" id="newmenubtn" class="btn btn-primary">Add Menu Items to Custom Menu</button>
+<button type="button" id="newmenubtn" class="btn btn-primary" style="margin:100px">Add Menu Items to Custom Menu</button>
 </form>
 
 <script>
