79b68fcf2ac675f11ea6a6ebbc4fbacbce522e44
arinamaros <b00128390@mytudublin.ie>
2023-03-31 05:50:32 +0100

    order page additions

--- resources/views/orderplaced.blade.php
+++ resources/views/orderplaced.blade.php
@@ -3,8 +3,9 @@
 
 
 <h1 style="text-align: center; margin-top:250px;"> Your Order Has Been Placed!</h1>
+<p style="text-align: center; font-weight: bold; font-size: 14pt;">We Look Forward To Making Your Event Come To Life</p>
 <p style="text-align: center;">
-<a href="{{ route('events.custindex', Auth::user()->customer->id) }}" class="btn btn-primary">View Orders</a>
+<a href="{{ route('events.custindex', Auth::user()->customer->id) }}" class="btn btn-primary">View Events</a>
 </p>
 
 
