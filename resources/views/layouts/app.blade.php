390ce179af569f76e9727b210635aae6d6cfdac3
arinamaros <b00128390@mytudublin.ie>
2023-04-02 13:21:14 +0100

    added favicon to tabs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
     <head> 
         <meta charset="UTF-8"> 
         <title>EventDream</title> 
+		<link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" type="image/x-icon">
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'> 
 
 		<!--suppress JSUnresolvedLibraryURL -->
