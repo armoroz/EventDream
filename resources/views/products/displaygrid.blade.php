9fcaa58f47ed1268191aa3f4cc43e5936809a912
arinamaros <b00128390@mytudublin.ie>
2023-04-21 12:12:59 +0100

    products again

--- resources/views/products/displaygrid.blade.php
+++ resources/views/products/displaygrid.blade.php
@@ -39,7 +39,7 @@
 						<div class="image-wrapper"><img  class="mx-auto d-block" src="{{ $product->productimg }}"/></div>
 					</div>
 						<div class="content-stdm">
-							<div class="card-header d-block"><h5 class="mx-auto d-block allproductnames {{$product->productname}}"></h5></div>
+							<div class="card-header d-block allproductnames"><h5 class="mx-auto d-block">{{$product->productname}}</h5></div>
 							<div class="card-footer" style="text-align: center">€{{$product->productcost}}</div>
 							<div class="card-footer"><button id="addItem" type="button" class="btn btn-addtoCart addItem" value="{{$product->id}}"><i class='far fa-shopping-cart'></i></button></div>
 							<div class="card-footer"><a  href="{{ route('products.custshow', [$product->id]) }}"><button id="custshow" type="button" class="btn btn-moreInfo custshow">More info <i class="fas fa-info-circle"></i></button></a></div>
