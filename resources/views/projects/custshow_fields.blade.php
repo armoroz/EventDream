437e5abce3862cb3516690b82dbbba5f4e113705
Shalini <B00128436@mytudublin.ie>
2023-04-28 00:42:58 +0100

    made changes

--- resources/views/projects/custshow_fields.blade.php
+++ resources/views/projects/custshow_fields.blade.php
@@ -1,66 +1,14 @@
 <!-- Eventdate Field -->
 <div class="col-sm-12">
-    {!! Form::label('eventdate', 'Eventdate:') !!}
+    {!! Form::label('eventdate', 'Project Date:') !!}
     <p>{{ $project->eventdate }}</p>
-</div>
-
-<!-- Eventtime Field -->
-<div class="col-sm-12">
-    {!! Form::label('eventtime', 'Eventtime:') !!}
-    <p>{{ $project->eventtime }}</p>
-</div>
-
-<!-- Orderplacedon Field -->
-<div class="col-sm-12">
-    {!! Form::label('orderplacedon', 'Orderplacedon:') !!}
-    <p>{{ $project->orderplacedon }}</p>
-</div>
-
-<!-- Eventordertotal Field -->
-<div class="col-sm-12">
-    {!! Form::label('eventordertotal', 'Eventordertotal:') !!}
-    <p>{{ $project->eventordertotal }}</p>
-</div>
-
-<!-- Eventdiscount Field -->
-<div class="col-sm-12">
-    {!! Form::label('eventdiscount', 'Eventdiscount:') !!}
-    <p>{{ $project->eventdiscount }}</p>
-</div>
-
-<!-- Numofguests Field -->
-<div class="col-sm-12">
-    {!! Form::label('numOfGuests', 'Numofguests:') !!}
-    <p>{{ $project->numOfGuests }}</p>
-</div>
-
-<!-- Venueid Field -->
-<div class="col-sm-12">
-    {!! Form::label('venueid', 'Venueid:') !!}
-    <p>{{ $project->venueid }}</p>
 </div>
 
 <!-- Customerid Field -->
 <div class="col-sm-12">
-    {!! Form::label('customerid', 'Customerid:') !!}
+    {!! Form::label('customerid', 'Customer ID:') !!}
     <p>{{ $project->customerid }}</p>
 </div>
 
-<!-- Userid Field -->
-<div class="col-sm-12">
-    {!! Form::label('userid', 'Userid:') !!}
-    <p>{{ $project->userid }}</p>
-</div>
 
-<!-- Standardmenuid Field -->
-<div class="col-sm-12">
-    {!! Form::label('standardmenuid', 'Standardmenuid:') !!}
-    <p>{{ $project->standardmenuid }}</p>
-</div>
-
-<!-- Custommenuid Field -->
-<div class="col-sm-12">
-    {!! Form::label('custommenuid', 'Custommenuid:') !!}
-    <p>{{ $project->custommenuid }}</p>
-</div>
 
