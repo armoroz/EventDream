265c940e4c6ab67c84728a4118a18d40d2cf0be3
arinamaros <b00128390@mytudublin.ie>
2023-04-27 15:42:32 +0100

    added project route to admin menu

--- resources/views/layouts/adminmenu.blade.php
+++ resources/views/layouts/adminmenu.blade.php
@@ -28,6 +28,9 @@
 		<li>
              <a class="dropdown-item" href="{!! route('eventproductlogs.index') !!}">Event Product Log</a>
         </li>
+		<li>
+             <a class="dropdown-item" href="{!! route('projects.index') !!}">Projects</a>
+        </li>
         <li>   
 			 <a class="dropdown-item" href="{!! route('products.index') !!}">Products</a>
         </li>
