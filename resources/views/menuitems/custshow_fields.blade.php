179e4813002eaff955598784b8e6a8e756950782
Shalini <B00128436@mytudublin.ie>
2023-05-04 23:40:11 +0100

    removed menu item carousel classes

--- resources/views/menuitems/custshow_fields.blade.php
+++ resources/views/menuitems/custshow_fields.blade.php
@@ -10,36 +10,7 @@
 
 			</div>
 			
-                @foreach($menuitems as $key => $menuitem)
-                    <input type="radio" name="slides" id="slide-{{ $key+1 }}" {{ $key === 0 ? 'checked="checked"' : '' }}>
-                @endforeach
-
-                <ul class="carousel__slides">
-                    @foreach($menuitems as $key => $menuitem)
-                        <li class="carousel__slide">
-                            <figure>
-                                <div style="height: 300px;">
-                                    <img style="box-shadow: 0 6px 21px rgba(0,0,0,1); height: 450px; width: 400px;" src="{{ $menuitem->menuitemimglink }}" alt="">
-                                </div>
-                                <figcaption>
-                                    
-                                </figcaption>
-                            </figure>
-                        </li>
-                    @endforeach
-                </ul>
-				
-                <ul class="carousel__thumbnails">
-                    @foreach($menuitems as $key => $menuitem)
-                        <li>
-                            <label for="slide-{{ $key+1 }}"><img src="{{ $menuitem->menuitemimglink }}" alt=""></label>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-
-            <div class="menuitem-details" style="margin-left: -30px;">
-			
+			<div style="margin-left: 50px;">
 				<!-- Menuitemname Field -->
 				<div class="col-sm-12">
 					<p style="font-size: 14pt;">Details for {{ $menuitem->menuitemname }}</p>
