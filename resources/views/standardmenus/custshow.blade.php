b37f29164ff06879b44f2b924169543a8d9ccffc
Shalini <B00128436@mytudublin.ie>
2023-05-04 20:48:00 +0100

    removed main heading for standard menu

--- resources/views/standardmenus/custshow.blade.php
+++ resources/views/standardmenus/custshow.blade.php
@@ -21,16 +21,6 @@
 
 
 </style>
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Standard Menu Details</h1>
-
-                </div>
-            </div>
-        </div>
-    </section>
 
     <div class="content px-3">
         <div class="card">
