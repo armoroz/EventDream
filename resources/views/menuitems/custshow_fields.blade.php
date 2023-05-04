2ea3c638114e5a79fabeb5553a5d54df9e5d1e14
Shalini <B00128436@mytudublin.ie>
2023-05-04 23:19:51 +0100

    rearranged fields for menu items

--- resources/views/menuitems/custshow_fields.blade.php
+++ resources/views/menuitems/custshow_fields.blade.php
@@ -29,37 +29,45 @@
                     @endforeach
                 </ul>
             </div>
-<!-- Menuitemname Field -->
-<div class="col-sm-12">
-    {!! Form::label('menuitemname', 'Menu Item Name:') !!}
-    <p>{{ $menuitem->menuitemname }}</p>
-</div>
 
-<!-- Description Field -->
-<div class="col-sm-12">
-    {!! Form::label('menuitemdesc', 'Description:') !!}
-    <p>{{ $menuitem->menuitemdesc }}</p>
-</div>
+            <div class="menuitem-details" style="margin-left: -30px;">
+			
+				<!-- Menuitemname Field -->
+				<div class="col-sm-12">
+					{!! Form::label('menuitemname', 'Menu Item Name:') !!}
+					<p>{{ $menuitem->menuitemname }}</p>
+				</div>
 
-<!-- Nutrition Field -->
-<div class="col-sm-12">
-    {!! Form::label('menuitemnutrition', 'Nutrition:') !!}
-    <p>{{ $menuitem->menuitemnutrition }}</p>
-</div>
+				<!-- Description Field -->
+				<div class="col-sm-12">
+					{!! Form::label('menuitemdesc', 'Description:') !!}
+					<p>{{ $menuitem->menuitemdesc }}</p>
+				</div>
 
-<!-- Allergens Field -->
-<div class="col-sm-12">
-    {!! Form::label('menuitemallergens', 'Allergens:') !!}
-    <p>{{ $menuitem->menuitemallergens }}</p>
-</div>
+				<!-- Nutrition Field -->
+				<div class="col-sm-12">
+					{!! Form::label('menuitemnutrition', 'Nutrition:') !!}
+					<p>{{ $menuitem->menuitemnutrition }}</p>
+				</div>
 
-<!-- Course Field -->
-<div class="col-sm-12">
-    {!! Form::label('course', 'Course:') !!}
-    <p>{{ $menuitem->course }}</p>
-</div>
+				<!-- Allergens Field -->
+				<div class="col-sm-12">
+					{!! Form::label('menuitemallergens', 'Allergens:') !!}
+					<p>{{ $menuitem->menuitemallergens }}</p>
+				</div>
 
-<!-- MenuItemimg Field -->
-<div><td><img class="img-responsive left-block" 
-	height="200px" width="200px" src="{{ $menuitem->menuitemimglink }}">
-</td></div>
+				<!-- Course Field -->
+				<div class="col-sm-12">
+					{!! Form::label('course', 'Course:') !!}
+					<p>{{ $menuitem->course }}</p>
+				</div>
+
+				
+            </div>
+        </div>
+    </div>
+</section>
+				<!-- MenuItemimg Field -->
+				<div><td><img class="img-responsive left-block" 
+					height="200px" width="200px" src="{{ $menuitem->menuitemimglink }}">
+				</td></div>
