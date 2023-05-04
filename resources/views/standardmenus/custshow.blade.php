07b0732720d57627e39dd148c4dc4030033ad405
Shalini <B00128436@mytudublin.ie>
2023-05-04 21:40:57 +0100

    removed buttons code and styling in standardmenu custshow.blade.php

--- resources/views/standardmenus/custshow.blade.php
+++ resources/views/standardmenus/custshow.blade.php
@@ -3,35 +3,12 @@
 @include('flash::message') 
 @php $j=0 @endphp 
 
-<style>
-.col-sm-6 {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.btn-giveRating {
-  margin-left: auto;
-}
-
-.btn-addtoCart {
-  margin-left: 10px;
-}
-
-
-
-</style>
 
     <div class="content px-3">
         <div class="card">
             <div class="card-body">
-			@include('standardmenus.custshow_fields')
-              <div class="row">
-                <div class="col-sm-6">    
-					<a href="{{ route('standardmenus.displaygrid') }}"><button class="btn btn-back"><i class='far fa-arrow-alt-up fa-9x fa-rotate-270'></i></button></a>
-			        <a href="{!! route('standardmenuratings.ratestandardmenu', [$standardmenu->id]) !!}"><button class="btn btn-giveRating"><i class='far fa-stars'></i></button></a>
-					<button id="addItem" type="button" class="btn btn-addtoCart addItem" value="{{$standardmenu->id}}"><i class='far fa-shopping-cart'></i></button>
-			    </div>		
+               <div class="row">
+      	       @include('standardmenus.custshow_fields')
                </div>
             </div>
         </div>
