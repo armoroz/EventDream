acfbc09322265992d406fd9b7d679990fc9f0abd
Shalini <B00128436@mytudublin.ie>
2023-05-04 20:35:12 +0100

    removed old standard menu carousel

--- resources/views/standardmenus/custshow_fields.blade.php
+++ resources/views/standardmenus/custshow_fields.blade.php
@@ -94,38 +94,3 @@ section {
 </style>
 
 
-
-<!-- Imagefile Field -->
-<div class="container-fluid">
-    <div id="thisCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#thisCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#thisCarousel" data-slide-to="1"></li>
-      <li data-target="#thisCarousel" data-slide-to="2"></li>
-    </ol>
-      <div id="thisCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Images -->
-        <div class="carousel-inner" role="listbox">	
-            @foreach($standardmenuimages as $standardmenuimage)
-            
-              <div style="padding-bottom:20px" class="item @if($loop->first) active @endif">
-                <img src="data:image/jpeg;base64,{{ $standardmenuimage->imagefile }}" 
-                    style="width:45%;height:350px;" class="img-responsive center-block">
-              </div>  
-            @endforeach
-        </div>
-        <!--controls -->
-        <a class="left carousel-control" href="#thisCarousel" data-slide="prev" style="background-image:none;color: black;">
-          <span class="glyphicon glyphicon-chevron-left"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#thisCarousel" data-slide="next" style="background-image:none;color: black;">
-          <span class="glyphicon glyphicon-chevron-right"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-</div>
-</div>
-
