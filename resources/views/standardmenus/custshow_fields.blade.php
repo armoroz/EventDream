4c3b89e8069431c28d4b4eb8ad103ec579c80298
Shalini <B00128436@mytudublin.ie>
2023-05-04 20:34:20 +0100

    added css

--- resources/views/standardmenus/custshow_fields.blade.php
+++ resources/views/standardmenus/custshow_fields.blade.php
@@ -73,7 +73,25 @@
     </div>
 </section>
 
+<style>
 
+.col-lg-8 {
+	min-width: 1000px;
+}
+
+.card {
+	background-color: transparent;
+}
+
+section {
+	box-shadow: 0 6px 21px rgba(0,0,0,1);
+}
+
+.card {
+	border: none;
+}
+
+</style>
 
 
 
