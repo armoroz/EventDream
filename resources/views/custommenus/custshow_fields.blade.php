86425af81cbaeed91ba9cafde4eaef3fd94d286f
Shalini <B00128436@mytudublin.ie>
2023-05-05 18:53:23 +0100

    made changes to custshow custom menu

--- resources/views/custommenus/custshow_fields.blade.php
+++ resources/views/custommenus/custshow_fields.blade.php
@@ -26,33 +26,42 @@ section {
 
 
 <section>
-	
-
-<div class="carousel-details-wrapper" style="display: flex; flex-wrap: wrap; margin-left: 30px;">				
-
-			
-  
-   <p style="font-size: 14pt;">Details for Custom Menu: {{ $custommenu->custommenuname }}</p>
+	<div class="container">
+        <div class="carousel-details-wrapper" style="display: flex; flex-wrap: wrap;">
+           <div class="content">		
+           <p style="font-size: 14pt;">Details for Custom Menu: {{ $custommenu->custommenuname }}</p>
     
   
 
 
-<table class="table table-condensed table-bordered">
-    <thead> 
-		<tr> <th>Name</th><th>Image</th><th>Price</th> </tr>
-	</thead>
-	<tbody>
-    {!! Form::label('menuitem', 'Menu Items:') !!}
-    @foreach($custommenu->menuitems as $menuitem)
-	<tr>
-        <td><i class='fad fa-check-circle fa-lg' style='color:#419b56'></i>{{ $menuitem->menuitemname }}</td>
-		<td><img class="img-responsive left-block" 
-	style="max-height: 100px; min-height: 100px; max-width:150px; min-width: 150px;" src="{{ $menuitem->menuitemimglink }}"></td>
-	<td>€{{$menuitem->menuitemcost}} </td>	 
-	</tr>
-	</tbody>
-    @endforeach
-</table>
-</div>
+			<table class="table table-condensed table-bordered">
+				<thead> 
+					<tr> <th>Name</th><th>Image</th><th>Price</th> </tr>
+				</thead>
+				<tbody>
+				{!! Form::label('menuitem', 'Menu Items:') !!}
+				@foreach($custommenu->menuitems as $menuitem)
+				<tr>
+					<td><i class='fad fa-check-circle fa-lg' style='color:#419b56'></i>{{ $menuitem->menuitemname }}</td>
+					<td><img class="img-responsive left-block" 
+				style="max-height: 100px; min-height: 100px; max-width:150px; min-width: 150px;" src="{{ $menuitem->menuitemimglink }}"></td>
+				<td>€{{$menuitem->menuitemcost}} </td>	 
+				</tr>
+				</tbody>
+				@endforeach
+			</table>
+			
+				<!-- Button Field -->
+				<div>
+					<a class="btn btn-primary" href="{{ route('standardmenus.displaygrid') }}">Back<i class='far fa-arrow-alt-up fa-9x fa-rotate-270' style='font-size: 1.2em; margin-left: 5px;'></i></a>
+			        <a class="btn btn-primary" href="{!! route('standardmenuratings.ratestandardmenu', [$standardmenu->id]) !!}">Rate Menu<i class='far fa-stars' style='font-size: 1.2em; margin-left: 3px;'></i></a>
+				    <button id="addItem" type="button" class="btn btn-primary" value="{{$standardmenu->id}}">Add to Cart<i class='far fa-shopping-cart' style='font-size: 1.em; margin-left: 3px;'></i></button>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+</section>
 
  
