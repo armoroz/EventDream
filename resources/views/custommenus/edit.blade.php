e8000b21bd2f0f934f98c86af37950f759f68679
Shalini <B00128436@mytudublin.ie>
2023-05-05 21:10:00 +0100

    changed css for create custom menu

--- resources/views/custommenus/edit.blade.php
+++ resources/views/custommenus/edit.blade.php
@@ -1,37 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>My Custom Menu</h1>
-                </div>
-            </div>
-        </div>
-    </section>
 
-    <div class="content px-3">
+<style>
 
-        @include('adminlte-templates::common.errors')
+.custom-menu-list {
+  display: block;
+  margin-bottom: 1rem;
+  text-align: left;
+  padding-left: 0;
+}
 
-        <div class="card" style="max-width: 400px">
+table.table-bordered {
+  border: 1px #999999;
+  
+}
 
-            {!! Form::model($custommenu, ['route' => ['custommenus.update', $custommenu->id], 'method' => 'patch']) !!}
+ul {
+list-style: none;
+padding: 0;
+margin: 0;
+}
 
-            <div class="card-body">
-                <div class="row">
-                    @include('custommenus.fields')
-                </div>
-            </div>
+section {
+	box-shadow: 0 6px 21px rgba(0,0,0,1);
+}
 
-            <div class="card-footer">
-                {!! Form::submit('Create Custom Menu', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('custommenus.displaygrid') }}" class="btn btn-default">Cancel</a>
-            </div>
 
-            {!! Form::close() !!}
+.card {
+	background-color: transparent;
+	border-color: transparent;
+	margin-top: 60px;
+	margin-left: 160px;
+}
 
-        </div>
-    </div>
+.inline-button {
+    display: inline-block;
+    margin-right: 10px; /* Optional: Add some spacing between the buttons */
+}
+
+.custom-menu-name {
+  display: block;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+</style>
+
+
+<section>
+	<div class="container">
+        <div class="carousel-details-wrapper" style="display: flex; flex-wrap: wrap;">
+		@include('adminlte-templates::common.errors')
+        {!! Form::model($custommenu, ['route' => ['custommenus.update', $custommenu->id], 'method' => 'patch']) !!}
+		<!-- Customerid Field -->
+        {!! Form::hidden('customerid', Auth::user()->customer->id) !!}
+		
+           <div class="content">		
+			   <p style="font-size: 14pt;">Create Custom Menu:</p>
+				<div class="form-group col-sm-12 col-lg-12">
+					<span class="custom-menu-name">{!! Form::text('custommenuname', null, ['class' => 'form-control','maxlength' => 20,'maxlength' => 20]) !!}</span>
+				</div>
+		
+				<table class="table table-condensed table-bordered">
+					<thead> 
+						<tr> <th>Name</th><th>Image</th><th>Price</th> </tr>
+					</thead>
+					<tbody>
+						{!! Form::label('menuitem', 'Menu Items:') !!}
+						@foreach($custommenu->menuitems as $menuitem)
+						<tr>
+							<td><i class='fad fa-check-circle fa-lg' style='color:#419b56'></i>{{ $menuitem->menuitemname }}</td>
+							<td><img class="img-responsive" 
+							style="max-height: 100px; min-height: 100px; max-width:150px; min-width: 150px; display: block; margin: auto;" src="{{ $menuitem->menuitemimglink }}"></td>
+							<td>€{{$menuitem->menuitemcost}} </td>	 
+						</tr>
+					</tbody>
+					@endforeach
+				</table>
+				
+				<!-- Button Field -->
+				<div>
+					{!! Form::submit('Create', ['class' => 'btn btn-primary']) !!}
+					<a href="{{ route('custommenus.displaygrid') }}" class="btn btn-default">Cancel</a>
+					{!! Form::close() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 @endsection
+ 
