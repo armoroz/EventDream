c248e76ca216eb33d7acf2052d68a46c4e41a393
Shalini <B00128436@mytudublin.ie>
2023-05-06 13:42:47 +0100

    changed button css btn primary 1

--- resources/views/standardmenus/custshow_fields.blade.php
+++ resources/views/standardmenus/custshow_fields.blade.php
@@ -72,7 +72,7 @@
 				<div>
 					<a class="btn btn-primary" href="{{ route('standardmenus.displaygrid') }}">Back<i class='far fa-arrow-alt-up fa-9x fa-rotate-270' style='font-size: 1.2em; margin-left: 5px;'></i></a>
 			        <a class="btn btn-primary" href="{!! route('standardmenuratings.ratestandardmenu', [$standardmenu->id]) !!}">Rate Menu<i class='far fa-stars' style='font-size: 1.2em; margin-left: 3px;'></i></a> 
-				    <button id="addItem" type="button" class="btn btn-primary addItem" value="{{$standardmenu->id}}"><span class="text">Add to Cart<i class='far fa-shopping-cart' style='font-size: 1.em; margin-left: 3px;'></i></span></button>
+				    <button id="addItem" type="button" class="btn btn-primary1 addItem" value="{{$standardmenu->id}}"><span class="text">Add to Cart<i class='far fa-shopping-cart' style='font-size: 1.em; margin-left: 3px;'></i></span></button>
 
 				</div>
             </div>
