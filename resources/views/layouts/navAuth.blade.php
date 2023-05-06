5af052d5228e6dc3ad8af0812373a9843c2fd03b
arinamaros <b00128390@mytudublin.ie>
2023-05-06 11:58:15 +0100

    css

--- resources/views/layouts/navAuth.blade.php
+++ resources/views/layouts/navAuth.blade.php
@@ -9,7 +9,7 @@
 	@else
 	<li class="nav-item dropdown">
 		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-			Sign Up
+			Sign In
 		</a>
 		<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 			<li>
