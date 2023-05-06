9b377dee9bf67f6efbffa6aa25a1995ac551da45
arinamaros <b00128390@mytudublin.ie>
2023-05-06 19:36:03 +0100

    css

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -115,7 +115,11 @@ div.scrollmenu a:hover {
 				<div class="content-stdm"> 
 					<div class="card-header d-block">{{ $product->productname }} {{ $product->productdesc }} {{ $product->producttype }}</div> 
 					<div class="card-footer" style="text-align: center; color:black;">€{{$product->productcost}}</div>			
-					<div class="card-footer"><a  href="{{ route('products.custshow', [$product->id]) }}"><button id="custshow" type="button" class="btn btn-primary1">Details <i class="fas fa-info-circle"></i></button></a></div>
+					<div class="card-footer">
+						<a href="{{ route('products.custshow', [$product->id]) }}" id="custshow" class="btn btn-primary1">
+							Details <i class="fas fa-info-circle"></i>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -139,7 +143,11 @@ div.scrollmenu a:hover {
 				<div class="content-stdm"> 
 					<div class="card-header d-block">{{ $standardmenu->standardmenuname }}</div> 
 					<div class="card-footer" style="text-align: center; color:black;">€20 Per Person</div>
-					<div class="card-footer"><a  href="{{ route('standardmenus.custshow', [$standardmenu->id]) }}"><button id="custshow"  type="button" class="btn btn-primary1">Details <i class="fas fa-info-circle"></i></button></a></div>
+					<div class="card-footer">
+						<a href="{{ route('standardmenus.custshow', [$standardmenu->id]) }}" id="custshow" class="btn btn-primary1">
+							Details <i class="fas fa-info-circle"></i>
+						</a>
+					</div>
 					<div class="card-footer"><input id="fieldRating" name="rating" 
 					value="{!! round($standardmenu->standardmenuratings->avg('rating'),2); !!}" 
 					type="hidden" data-theme="krajee-fas" class="rating rating-loading" data-min=0 
