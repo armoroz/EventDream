ad982025b70e91525cfccad6932c37e089075c9b
arinamaros <b00128390@mytudublin.ie>
2023-05-06 19:04:16 +0100

    menus

--- resources/views/layouts/navAuth.blade.php
+++ resources/views/layouts/navAuth.blade.php
@@ -8,7 +8,7 @@
 		@include('layouts.profilemenu')
 	@else
 	<li class="nav-item dropdown">
-		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+		<a class="nav-link dropdown-toggle" style="margin-right: -10px;" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 			Sign In
 		</a>
 		<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
