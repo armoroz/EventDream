79f5350fc25c88845b6fe70fa723ad22b3587bab
arinamaros <b00128390@mytudublin.ie>
2023-05-06 13:26:55 +0100

    about us

--- resources/views/aboutus/index.blade.php
+++ resources/views/aboutus/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-<h1 style="text-align: center; margin-top: 20px; font-family: Garamond, serif;">About Us</h1>
+<h1 style="text-align: center; margin-top: 20px; font-family: Garamond, serif; font-size: 36pt;">About Us</h1>
 
   <section>
   <div>
@@ -31,9 +31,49 @@
     </ul>
   </div>
 
+  
+	<div id="photoCarousel" class="carousel slide" data-bs-ride="carousel">
+	<h2 style="margin: 20px 0px 20px 0px;">Our Work</h2>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="{{asset('img\venue.jpg')}}" class="d-block w-100" alt="Venue 1">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue5.jpg')}}" class="d-block w-100" alt="Venue 2">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue6.jpg')}}" class="d-block w-100" alt="Venue 3">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue7.jpg')}}" class="d-block w-100" alt="Venue 4">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue8.jpg')}}" class="d-block w-100" alt="Venue 5">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue4.jpg')}}" class="d-block w-100" alt="Venue 6">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue1.jpg')}}" class="d-block w-100" alt="Venue 7">
+			</div>
+			<div class="carousel-item">
+				<img src="{{asset('img\venue2.jpg')}}" class="d-block w-100" alt="Venue 8">
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#photoCarousel" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#photoCarousel" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+
   <footer>
-    <p>Event Dream. All rights reserved.</p>
+    <p style="text-align: center;">Event Dream. All rights reserved.</p>
   </footer>
+
 </section>
 
 <style>
