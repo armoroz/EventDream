dc37f7ba79f016f4ecba888e71b96de961d2c92e
Shalini <B00128436@mytudublin.ie>
2023-05-07 13:32:13 +0100

    changed permission admin

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -8,14 +8,16 @@
                     <h1>Permissions</h1>
                 </div>
                 <div class="col-sm-6">
-                    <a class="btn btn-primary float-right"
-                       href="{{ route('permissions.create') }}">
-                        Add New
-                    </a>
                 </div>
             </div>
         </div>
     </section>
+
+@section('side2')
+<div class="button-container">
+    <a href="{{ route('permissions.create') }}" class="btn btn-primary1 fixed-button">New Permission<i class="fas fa-plus-circle" style="font-size: 1.2em; margin-right: 5px;"></i></a>
+</div>
+@endsection('side2')
 
     <div class="content px-3">
 
