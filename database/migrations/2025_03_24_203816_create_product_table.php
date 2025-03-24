a197e8a66b1f3ca2b7ce164ca401e23f6a3acb26
arinamaros <b00128390@mytudublin.ie>
2025-03-24 21:23:13 +0000

    db

--- database/migrations/2025_03_24_203816_create_product_table.php
+++ database/migrations/2025_03_24_203816_create_product_table.php
@@ -10,20 +10,32 @@ class CreateProductTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */
+     
     public function up()
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
-    }
+    }*/
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
+	 
+	public function up()
+	{
+		Schema::create('product', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->decimal('price', 8, 2);
+			$table->text('description')->nullable();
+			$table->timestamps();
+		});
+	}	 
+	 
     public function down()
     {
         Schema::dropIfExists('product');
